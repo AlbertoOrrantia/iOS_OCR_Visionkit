3198c67c12c92d3b047a017ae4e57ee591b8ba64
Alberto Orrantia <ricco.albert@gmail.com>
2024-01-05 13:24:57 -0600

    Initial Commit

---
+++ OCR_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OCR_iOS
+//
+//  Created by Alberto Orrantia on 05/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OCR_iOS/OCR_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCR_iOSApp.swift
+//  OCR_iOS
+//
+//  Created by Alberto Orrantia on 05/01/24.
+//
+
+import SwiftUI
+
+@main
+struct OCR_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

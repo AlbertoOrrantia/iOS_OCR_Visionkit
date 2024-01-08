17d67d3591e52763fd699eb6d1bb128b59c5ab21
Alberto Orrantia <ricco.albert@gmail.com>
2024-01-08 00:34:46 -0600

    iOS_OCR_VisionKit

--- OCR_iOS/ContentView.swift
+++ OCR_iOS/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var scanProvider = ScanProvider()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ScanView(scanProvider: scanProvider)
+            .sheet(isPresented: $scanProvider.showSheet) {
+                VStack(alignment: .leading) {
+                    Text(scanProvider.text)
+                        .font(.system(.body, design: .rounded))
+                        .padding(.top, 20)
+                        .padding(.horizontal, 20)
+                    Spacer()
+                }
+                .presentationDragIndicator(.visible)
+                .presentationDetents([.medium, .large])
+            }
     }
 }
 
--- OCR_iOS/ScanView.swift
+++ OCR_iOS/ScanView.swift
@@ -6,13 +6,43 @@
 //
 
 import SwiftUI
+import VisionKit
 
-struct ScanView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct ScanView: UIViewControllerRepresentable {
+    @ObservedObject var scanProvider: ScanProvider
+    func makeUIViewController(context: Context) -> DataScannerViewController {
+        //Here we can change the accuracy in reltion to speed
+        let dataScannerViewController = DataScannerViewController(recognizedDataTypes: [.text()], qualityLevel: .balanced, isHighlightingEnabled: true)
+        dataScannerViewController.delegate = scanProvider
+        try? dataScannerViewController.startScanning()
+        return dataScannerViewController
+    }
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
     }
 }
 
-#Preview {
-    ScanView()
+final class ScanProvider: NSObject, DataScannerViewControllerDelegate, ObservableObject {
+    @Published var text: String = ""
+    @Published var error: DataScannerViewController.ScanningUnavailable?
+    @Published var showSheet:Bool = false
+    
+    func dataScanner(_ dataScanner: DataScannerViewController, didTapOn item: RecognizedItem) {
+        switch item {
+        case .text(let recognizedText):
+            self.text = recognizedText.transcript
+            self.showSheet.toggle()
+            print(recognizedText)
+        case .barcode(_):
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    func dataScanner(_ dataScanner: DataScannerViewController, 
+                     becameUnavailableWithError error: DataScannerViewController.ScanningUnavailable) {
+        self.error = error
+        print (error)
+    }
 }

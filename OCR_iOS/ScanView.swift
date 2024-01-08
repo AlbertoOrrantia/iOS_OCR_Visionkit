42427abb75f6b575b8cc9ddc9280e32688d32924
Alberto Orrantia <ricco.albert@gmail.com>
2024-01-08 00:32:17 -0600

    first commit

---
+++ OCR_iOS/ScanView.swift
@@ -0,0 +1,18 @@
+//
+//  ScanView.swift
+//  OCR_iOS
+//
+//  Created by Alberto Orrantia on 05/01/24.
+//
+
+import SwiftUI
+
+struct ScanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanView()
+}
